2be910679169fdc06e93a9fec61613bcc7edd7aa
Kwasi Amponsah <amponsks@mail.uc.edu>
2023-11-02 21:47:46 -0400

    Add project files.

---
+++ MyWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Your Name" });
+        }
+    }
+}
